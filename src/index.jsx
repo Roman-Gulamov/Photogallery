6654d6fc50fc3a0a6563a9654eeb257446c5bb12
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-11 03:52:06 +0300

    Added fonts

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 
+import './assets/fonts/arbutus/stylesheet.css';
+
 
 ReactDOM.render(<App />,document.getElementById('root'));
