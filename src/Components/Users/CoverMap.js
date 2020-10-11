46eab3bbad67098a53501325b2c8cc1f5b96325a
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-11 03:55:14 +0300

    Array with images for users

---
+++ src/Components/Users/CoverMap.js
@@ -0,0 +1,54 @@
+import Bret from '../../assets/images/Bret.jpg';
+import Antonette from '../../assets/images/Antonette.jpg';
+import Samantha from '../../assets/images/Samantha.jpg';
+import Karianne from '../../assets/images/Karianne.jpg';
+import Kamren from '../../assets/images/Kamren.jpg';
+import Leopoldo from '../../assets/images/Leopoldo.jpg';
+import Elwyn from '../../assets/images/Elwyn.jpg';
+import Maxime from '../../assets/images/Maxime.jpg';
+import Delphine from '../../assets/images/Delphine.jpg';
+import Moriah from '../../assets/images/Moriah.jpg';
+
+
+export const COVER_MAP = [
+    {
+        imageId: 1,
+        image: Bret
+    },
+    {
+        imageId: 2,
+        image: Antonette
+    },
+    {
+        imageId: 3,
+        image: Samantha
+    },
+    {
+        imageId: 4,
+        image: Karianne
+    },
+    {
+        imageId: 5,
+        image: Kamren
+    },
+    {
+        imageId: 6,
+        image: Leopoldo
+    },
+    {
+        imageId: 7,
+        image: Elwyn
+    },
+    {
+        imageId: 8,
+        image: Maxime
+    },
+    {
+        imageId: 9,
+        image: Delphine
+    },
+    {
+        imageId: 10,
+        image: Moriah
+    },
+]
