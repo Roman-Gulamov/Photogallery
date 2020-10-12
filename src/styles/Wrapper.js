0216b6d346f01b619581ff6556891b9b05308f58
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-12 05:53:08 +0300

    Styles for the wrapper

---
+++ src/styles/Wrapper.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    text-align: center;
+    margin-bottom: 5vh;
+` 
