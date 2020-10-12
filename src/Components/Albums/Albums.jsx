2659fbc2088fbd2600a9faf8b4765a7f20410738
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-12 05:50:33 +0300

    Finished writing the Albums component

--- src/Components/Albums/Albums.jsx
+++ src/Components/Albums/Albums.jsx
@@ -1,20 +1,41 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+
+import { Back } from '../Back';
+import { Wrapper } from '../../styles/Wrapper';
+import {
+    Card,
+    CardItem,
+    ItemText,
+    ItemHover,
+    Hover
+} from '../../styles/Card';
 
 
 export const Albums = ({ parseAlbums, propsId, username }) => {
     return (
         <>
-        {parseAlbums.map(({ userId, id, title }) =>
-            userId === propsId ?
-            <Fragment key={id}>
-                <div>Title: {title}</div>
-                <div>UserID: {userId}</div>
-                <Link to={`/users/${username}/album-${id}`}>
-                    <div>ID: {id}</div>
-                </Link>
-            </Fragment>  : null
-        )}
+        <Back path='/users' />
+        <Wrapper>
+            {parseAlbums.map(({ userId, id, title, photosCount, image }) =>
+                userId === propsId ?
+                <Card key={id}>
+                    <CardItem background={image}>
+                        <ItemText updateItem>{title}</ItemText>
+                        <ItemText updateItem>({photosCount} photos)</ItemText>
+                        <ItemHover>
+                            <Hover>
+                                <Link to={`/users/${username}/album-${id}`}>
+                                    <FontAwesomeIcon icon={faEye} size="3x" />
+                                </Link>
+                            </Hover>
+                        </ItemHover>
+                    </CardItem>
+                </Card> : null
+            )}
+        </Wrapper>
         </>
     )
 }
