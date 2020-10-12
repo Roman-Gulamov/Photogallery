8d48857ffb6bae60466f72e9be2937704ab37e95
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-12 05:50:27 +0300

    Finished writing the Users component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,44 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
 
+import { Wrapper } from '../../styles/Wrapper';
 import {
-    UsersMessage,
-    UsersWrapper,
-    UsersCard,
+    Card,
     CardItem,
     ItemText,
     ItemHover,
-    HoverEye
-} from '../../styles/Users';
+    Hover
+} from '../../styles/Card';
 
 
-export const Users = ({ ParseUsers, error, loading }) => {
-    if (error) {
-        return <UsersMessage>Error: {error.message}</UsersMessage>
-    } else if (!loading) {
-        return <UsersMessage>Please wait...</UsersMessage>
-    } else {
-        return (
-            <UsersWrapper>
-                {ParseUsers.map(({ id, name, username, image, albums }) =>
-                    <UsersCard key={id}>
-                        <CardItem background={image}>
-                            <ItemText>{name}</ItemText>
-                            <ItemText>({username})</ItemText>
-                            <ItemText>{albums} albums</ItemText>
-                            <ItemHover>
-                                <HoverEye>
-                                    <Link to={`/users/${username}`}>
-                                        <FontAwesomeIcon icon={faEye} size="3x" />
-                                    </Link>
-                                </HoverEye>
-                            </ItemHover>
-                        </CardItem>
-                    </UsersCard>
-                )}
-            </UsersWrapper>
-        )
-    }   
+export const Users = ({ parseUsers }) => {
+    return (
+        <Wrapper>
+            {parseUsers.map(({ id, name, username, image, albumsCount }) =>
+                <Card key={id}>
+                    <CardItem background={image}>
+                        <ItemText>{name}</ItemText>
+                        <ItemText users>({username})</ItemText>
+                        <ItemText users>{albumsCount} albums</ItemText>
+                        <ItemHover>
+                            <Hover>
+                                <Link to={`/users/${username}`}>
+                                    <FontAwesomeIcon icon={faFolderOpen} size="3x" />
+                                </Link>
+                            </Hover>
+                        </ItemHover>
+                    </CardItem>
+                </Card>
+            )}
+        </Wrapper>
+    )
 }
