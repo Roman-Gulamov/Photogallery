812eba1e56b340a37d7957d6c5fa6e65893c5cf9
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-12 05:52:45 +0300

    Styles for popup photo gallery

---
+++ src/styles/PopUp.js
@@ -0,0 +1,47 @@
+import styled, { css } from 'styled-components';
+
+export const PopUpWrapper = styled.div`
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    
+    ${props => props.active && css`
+        display: block;
+    `}
+
+    & div svg {
+        cursor: pointer;
+        transition: transform .4s ease;
+
+        &:hover {
+            transform: scale(0.9);
+        }
+    } 
+` 
+
+export const PopUpClose = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-right: 2em;
+` 
+
+export const PopUpCarousel = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 0 1em 3em;
+` 
+
+export const CarouselImg = styled.div`
+    display: flex;
+    justify-content: center;
+` 
+
+export const ImgActive = styled.img`
+    width: 80%;
+` 
