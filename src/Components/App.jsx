ffd2c34b049aad6e973e068421c43e30d3fc38ac
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-12 05:50:57 +0300

    Added new input values to the array

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
-import { 
-    HashRouter as Router, 
-    Switch, 
-    Route,
-    Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { GlobalStyle } from '../styles/GlobalStyle';
 import { Container } from '../styles/Container';
+import { Message } from '../styles/Message';
 
 import { COVER_MAP } from './Users/CoverMap';
 import { Header } from './Header/Header';
@@ -29,10 +26,18 @@ const App = () => {
         id,
         name,
         username,
-        albums: parseAlbums.filter(item => item.userId === id).length,
+        albumsCount: parseAlbums.filter(item => item.userId === id).length,
         image: COVER_MAP.filter(item => item.imageId === id).map(({ image }) => image).join()
     }));
 
+    const newParseAlbums = parseAlbums.map(({ userId, id, title }) => ({
+        userId,
+        id,
+        title,
+        photosCount: parsePhotos.filter(item => item.albumId === id).length,
+        image: parsePhotos.filter(item => item.albumId === id).splice(0,1).map(({ thumbnailUrl }) => thumbnailUrl).join()
+    }));
+    
     useEffect(() => {
         PATHS.map((path) =>
             fetch(`https://jsonplaceholder.typicode.com/${path}`)
@@ -40,7 +45,7 @@ const App = () => {
             .then(
                 (result) => {
                     setLoading(true);
-                    path === 'albums' ? 
+                    path === 'albums' ?  
                         setParseAlbums(result) : 
                     path === 'users' ? 
                         setParseUsers(result) : 
@@ -53,53 +58,63 @@ const App = () => {
         )
     }, [])
 
-    return (
-        <>
-        <GlobalStyle />
-        <Router>
-            <Header />
-            <Container>
-                <Switch>
-                    <Route 
-                        exact 
-                        path={["/", "/users"]}
-                        component={() => 
-                            <Users 
-                                ParseUsers={newParseUsers} 
-                                error={error} 
-                                loading={loading} 
+    if (error) {
+        return (
+            <>
+            <GlobalStyle />
+                <Message>Error: {error.message}</Message>
+            </>
+        )
+    } else if (!loading) {
+        return (
+            <>
+            <GlobalStyle />
+                <Message>Please wait...</Message>
+            </>
+        )
+    } else {
+        return (
+            <>
+            <GlobalStyle />
+            <Router>
+                <Header />
+                <Container>
+                    <Switch>
+                        <Route 
+                            exact 
+                            path={["/", "/users"]}
+                            component={() => <Users parseUsers={newParseUsers} />}
+                        />
+                        {parseUsers.map(({ id, username }) =>
+                            <Route 
+                                key={id} 
+                                exact
+                                path={`/users/${username}`} 
+                                component={() => 
+                                    <Albums 
+                                        parseAlbums={newParseAlbums} 
+                                        propsId={id} 
+                                        username={username} 
+                                    />
+                                } 
                             />
-                        }
-                    />
-                    {parseUsers.map(({ id, username }) =>
-                        <Route 
-                            key={id} 
-                            exact
-                            path={`/users/${username}`} 
-                            component={() => 
-                                <Albums 
-                                    parseAlbums={parseAlbums} 
-                                    propsId={id} 
-                                    username={username} 
-                                />
-                            } 
-                        />
-                    )}
-                    {parseAlbums.map(({ id }) =>
-                    parseUsers.map(({ username }) =>
-                        <Route 
-                            key={id} 
-                            path={`/users/${username}/album-${id}`} 
-                            component={() => <Photos parsePhotos={parsePhotos} propsId={id} username={username} />} 
-                        />
-                    ))}
-                    <Route component={Error} />
-                    <Redirect to="/" />
-                </Switch>
-            </Container>
-        </Router> 
-        </>
-    )
+                        )}
+                        {parseAlbums.map(({ id }) =>
+                        parseUsers.map(({ username }) =>
+                            <Route 
+                                key={id}
+                                exact
+                                path={`/users/${username}/album-${id}`} 
+                                component={() => <Photos parsePhotos={parsePhotos} propsId={id} username={username} />} 
+                            />
+                        ))}
+                        <Route component={Error} />
+                    </Switch>
+                </Container>
+            </Router> 
+            </>
+        )
+    }
 }
 
 export default App;
